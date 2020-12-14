6fec34f33bc6bdf9f3aa774b02302148acacc764
Patrik Oravecz <acm9597@gmail.com>
2020-12-14 21:07:19 +0100

    olvasoosztaly

--- Projekt/Projekt/Form1.cs
+++ Projekt/Projekt/Form1.cs
@@ -18,21 +18,6 @@ namespace Projekt
 
             InitializeComponent();
         }
-
-        public class Reader
-        {
-            static void Main(string[] args)
-            {
-                    StreamReader reader = new StreamReader("C:\\Sample.txt", Encoding.Default);
-                string szoveg;
-                while (!reader.EndOfStream)
-                {
-                    szoveg = reader.ReadLine();
-                    Console.WriteLine(szoveg);
-                }
-                reader.Close();
-                Console.ReadLine();
-            }
-        }
+        
     }
 }
---
+++ Projekt/Projekt/Reader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt
+{
+    class Reader
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("C:\\Sample.txt", Encoding.Default);
+            string szoveg;
+            while (!reader.EndOfStream)
+            {
+                szoveg = reader.ReadLine();
+                Console.WriteLine(szoveg);
+            }
+            reader.Close();
+            Console.ReadLine();
+        }
+    }
+}
